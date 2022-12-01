2a0b5d20bef2b0f0b0f249744a5afaeeb7668a55
Joel Onwuanaku <joelebuka@gmail.com>
2022-12-01 16:20:47 +0100

    Made navbar responsive

--- frontend/src/components/global/layouts/Navbar.js
+++ frontend/src/components/global/layouts/Navbar.js
@@ -16,35 +16,47 @@ export default function Navbar() {
   };
   return (
     <Container className={'max-width w-[100%]'}>
-      <nav className={open ? `h-[100vh] [w-100%]` : `py-[3.2rem] w-[100%]  md:flex items-center justify-between`}>
-        <div className="mb-[3.2rem] md:mb-0 ">
+      <nav
+        className={
+          open
+            ? `absolute z-[10] left-0 bg-black h-[100vh] w-full p-[3.2rem]`
+            : `py-[3.2rem] w-[100%]  md:flex items-center justify-between`
+        }
+      >
+        <div className="flex justify-between mb-[3.2rem] md:mb-0 ">
           <Link href="/">
             <img className="h-[4rem]" src="/images/icon-logo.png" alt="" />
           </Link>
+
+          <svg onClick={toggle} className="h-[3.2rem] w-[3.2rem] md:hidden">
+            <use href="/images/sprite.svg#icon-menu" />
+          </svg>
         </div>
 
-        <div className="flex flex-col gap-y-[2.4rem] md:gap-y-0 md:items-center md:flex-row md:gap-x-[8rem]">
+        <div
+          className={open ? 'flex flex-col gap-y-[4rem] md:gap-y-0' : 'hidden md:flex md:items-center md:gap-x-[8rem]'}
+        >
           <ul>
             <Link href="/">
-              <li>
+              <li className={pathname === '/' ? 'text-[#FF9900] md:text-white' : 'text-white'}>
                 Home
                 <hr className={pathname === '/' ? 'visible' : 'invisible'} />
               </li>
             </Link>
             <Link href="/about">
-              <li>
+              <li className={pathname === '/about' ? 'text-[#FF9900] md:text-white' : 'text-white'}>
                 About
                 <hr className={pathname === '/about' ? 'visible' : 'invisible'} />
               </li>
             </Link>
             <Link href="/listing">
-              <li>
+              <li className={pathname === '/listing' ? 'text-[#FF9900] md:text-white' : 'text-white'}>
                 Listing
                 <hr className={pathname === '/listing' ? 'visible' : 'invisible'} />
               </li>
             </Link>
             <Link href="/contact">
-              <li>
+              <li className={pathname === '/contact' ? 'text-[#FF9900] md:text-white' : 'text-white'}>
                 Contact Us
                 <hr className={pathname === '/contact' ? 'visible' : 'invisible'} />
               </li>

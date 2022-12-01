479684b77d8f35109b7b96c8eba2ac84468fd340
Joel Onwuanaku <joelebuka@gmail.com>
2022-12-01 17:42:51 +0100

    Fixed Clashing CSS

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,3 @@
-// import '../styles/main.scss';
-import { AuthCheck } from '@/global//components/AuthCheck';
 import { useRouter } from 'next/router';
 import { Provider } from 'react-redux';
 import store from 'store/store';
@@ -8,15 +6,22 @@ function MyApp({ Component, pageProps }) {
   const pathname = useRouter().pathname;
   if (pathname === '/admin' || pathname.includes('admin')) {
     require('../assets/styles/admin.scss');
-  } else {
+  }
+  if (
+    pathname === '/' ||
+    pathname === '/listing' ||
+    pathname.includes('listing') ||
+    pathname === '/about' ||
+    pathname.includes('about') ||
+    pathname === 'contact' ||
+    pathname.includes('contact')
+  ) {
     require('../assets/styles/global.scss');
   }
 
   return (
     <Provider store={store}>
-      {/* <AuthCheck> */}
       <Component {...pageProps} />
-      {/* </AuthCheck> */}
     </Provider>
   );
 }
